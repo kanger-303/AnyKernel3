8e3dfe2d63ffa8822d35b0a45fc2481faef50118
kanger-303 <kanger303@gmail.com>
2025-05-17 20:22:55 +0700

    Anykernel3: update
    Signed-off-by: kanger-303 <kanger303@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernol developer @Zyusz
+kernel.string=Kernol developer @AmbaTaull
 do.devicecheck=1
 do.modules=0
 do.systemless=1
